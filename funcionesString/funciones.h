66c0e9b2d5dfa98f6b94715d5a20648db86e9345
Hernan Ferreras <hernanferreras@icloud.com>
2026-09-08 14:05:16 -0300

    Add new string manipulation function prototypes

--- funcionesString/funciones.h
+++ funcionesString/funciones.h
@@ -2,6 +2,8 @@
 #define FUNCIONES_H_INCLUDED
 
 #include <stdbool.h>
+#include <stddef.h>
+
 
 typedef struct
 {
@@ -12,6 +14,12 @@ typedef struct
 
 int miStrlen(const char* cad);
 int miStrcmp(const char* cad1, const char* cad2);
-
+int miStrcmpi(const char* cad1, const char* cad2);
+char* miStrcpy(char* cadDest, const char* cadOrig);
+char* miStrncpy(char* cadDest, const char* cadOrig, size_t n);
+char* miStrstr(const char* cad, const char* subCad);
+char* miStrchr(const char* cad, char c);
+char* miStrcat(char* cad1, const char* cad2);
+char* miStrncat(char* cad1, const char* cad2, size_t n);
 
 #endif // FUNCIONES_H_INCLUDED
