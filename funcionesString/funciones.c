034390bee1d5830bc669694f2c165dbfdedd9470
Hernan Ferreras <hernanferreras@icloud.com>
2026-09-09 22:39:12 -0300

    Improve string comparison and lowercase conversion
    Refactor miStrcmpi to use cadenaAMinuscula for case-insensitive comparison and fix cadenaAMinuscula implementation.

--- funcionesString/funciones.c
+++ funcionesString/funciones.c
@@ -1,13 +1,18 @@
+#include <stdio.h>
 #include "funciones.h"
 
-#define esMayuscula(C) (((C) >= 'A') && ((C) <= 'Z'))
 
+#define esMayuscula(C) (((C) >= 'A') && ((C) <= 'Z'))
+#define TAM 100
+
+void cadenaAMinuscula(char* puntero, const char* cadOrig, char* cadMinuscula);
 /*void secuenciaPalabraCrear(secuenciaPalabra* sec, const char* c)
 {
     sec->cursor = c;
     sec->finSec = false;
 
 }*/
+
 
 int miStrlen(const char* cad)
 {
@@ -49,20 +54,37 @@ int miStrcmp(const char* cad1, const char* cad2)
 
 int miStrcmpi(const char* cad1, const char* cad2)
 {
-    char* puntACad1 = cad1;
-    char* puntACad2 = cad2;
-    bool flag = false;
+    char* puntACad1 = (char*) cad1;
+    char* puntACad2 = (char*) cad2;
+    char cadena1[TAM];
+    char cadena2[TAM];
+    
+    cadenaAMinuscula(puntACad1, cad1, cadena1);
+    cadenaAMinuscula(puntACad2, cad2, cadena2);
 
-    while((*puntACad1) != '\0' || (*puntACad2) != '\0')
-    {
-        if(esMayuscula())
-
-    }
-
+    printf("%s\n", cadena1);
+    printf("%s\n", cadena2);
+    return miStrcmp(cadena1, cadena2);
+    
 }
-char* miStrcpy(char* cadDest, const char* cadOrig);
-char* miStrncpy(char* cadDest, const char* cadOrig, size_t n);
-char* miStrstr(const char* cad, const char* subCad);
-char* miStrchr(const char* cad, char c);
-char* miStrcat(char* cad1, const char* cad2);
-char* miStrncat(char* cad1, const char* cad2, size_t n);
+
+void cadenaAMinuscula(char* puntero, const char* cadOrig, char* cadMinuscula)
+{
+    int i;
+    while(*puntero != '\0')
+    {
+        for(i = 0; i < miStrlen(cadOrig); i++)
+        {
+            if(esMayuscula(*puntero))
+            {
+                *cadMinuscula = *puntero + 32;
+            } else
+            {
+                *cadMinuscula = *puntero;
+            }
+
+            puntero++;
+            cadMinuscula++;
+        }
+    }
+}
