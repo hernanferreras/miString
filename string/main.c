0f1210097d277b96059341220c724494c27a7203
Hernan Ferreras <hernanferreras@icloud.com>
2026-09-09 22:40:07 -0300

    Modify string values and add case-insensitive comparison
    Updated string values in cad1 and cad2. Added case-insensitive string comparison test.

--- string/main.c
+++ string/main.c
@@ -8,12 +8,10 @@
 int main()
 {
 
-    char cad1[TAM] = "PRUEBA DE CADENA";
-    char cad2[TAM] = "Prueba de Cadena";
+    char cad1[TAM] = "Prueba de Candena";
+    char cad2[TAM] = "prueba de CANDena";
 
-    printf("CADENA 1: %s\n", cad1);
-    printf("CADENA 2: %s\n\n", cad2);
-    //PRUEBA DE STRLEN
+    // PRUEBA DE STRLEN
     printf("STRLEN: La cantidad de caracteres de la cadena es de %d caracteres.\n", miStrlen(cad1));
 
     //PRUEBA DE STRCMP
@@ -22,6 +20,14 @@ int main()
     } else
     {
         printf("STRCMP: Las cadenas son diferentes (%d)\n", miStrcmp(cad1, cad2));
+    }
+
+    //PRUEBA DE STRCMPI
+    if(miStrcmpi(cad1, cad2) == 0){
+        printf("STRCMP: Las cadenas son iguales\n");
+    } else
+    {
+        printf("STRCMP: Las cadenas son diferentes (%d)\n", miStrcmpi(cad1, cad2));
     }
 
     return 0;
